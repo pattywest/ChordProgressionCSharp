8f8f555f009c6811d93a9decd579bf9e156afc45
pat west <patwest@iastate.edu>
2022-10-31 17:02:00 -0500

    Finished basic random chord progression

--- ChordProgression/ChordProgression/Program.cs
+++ ChordProgression/ChordProgression/Program.cs
@@ -20,15 +20,15 @@ namespace ChordProgression
             chordsCanGoTo = canGoTo;
         }
 
-        public String nextChord(){
+        public Chords nextChord(){
             if (chordsCanGoTo == null)
             {
                 Console.WriteLine("No Chords to go to. Please use setChordsCanGoTo method to set the chords");
                 return null;
             }
             var rand = new Random();
-            int next = rand.Next(chordsCanGoTo.Length + 1);
-            return chordsCanGoTo[next].toString();
+            int next = rand.Next(chordsCanGoTo.Length);
+            return chordsCanGoTo[next];
         }
 
         public String toString()
@@ -64,11 +64,11 @@ namespace ChordProgression
 
             IMaj.setChordsCanGoTo(new Chords[] { IMaj, iiMaj, iiiMaj, IVMaj, VMaj, viMaj, viiMaj });
             iiMaj.setChordsCanGoTo(new Chords[] { viiMaj, VMaj });
-            iiiMaj.setChordsCanGoTo(new Chords[] { iiMaj, IVMaj, viMaj});
+            iiiMaj.setChordsCanGoTo(new Chords[] { iiMaj, IVMaj, viMaj });
             IVMaj.setChordsCanGoTo(new Chords[] { IMaj, iiMaj, VMaj, viiMaj });
             VMaj.setChordsCanGoTo(new Chords[] { IMaj, viMaj });
             viMaj.setChordsCanGoTo(new Chords[] { IMaj, iiMaj, IVMaj, VMaj, viiMaj });
-            viiMaj.setChordsCanGoTo(new Chords[] { IMaj, VMaj, viMaj});
+            viiMaj.setChordsCanGoTo(new Chords[] { IMaj, VMaj, viMaj });
 
             chordMapMaj.Add("I", IMaj);
             chordMapMaj.Add("ii", iiMaj);
@@ -112,96 +112,121 @@ namespace ChordProgression
             chordMapMin.Add("VI", VIMin);
             chordMapMin.Add("viid", viiMin);
 
-            int numOfChords = 0;
-            Boolean isMajor = true;
-            String startingChord = "";
+            Boolean keepRunning = true;
             Console.WriteLine("Welcome to the random (but correct) Triad Chord Progression in Roman Numerals!");
-            Console.WriteLine("How many Chords do you want?");
-            numOfChords = Convert.ToInt32(Console.ReadLine());
-            String[] chordProg = new String[numOfChords];
-            Boolean continueWhile = true;
-            while (continueWhile)
+            while (keepRunning)
             {
-                Console.WriteLine("Do you want Major triads, or Minor triads? Type 'Major' or 'Minor' depending on what you want");
-                String majorOrMinor = Console.ReadLine();
-                if (majorOrMinor.Equals("Major") || majorOrMinor.Equals("major"))
+                int numOfChords = 0;
+                Boolean isMajor = true;
+                String startingChord = "";
+                while (numOfChords == 0)
                 {
-                    isMajor = true;
-                    continueWhile = false;
+                    Console.WriteLine("How many Chords do you want? Must have at least 3.");
+                    numOfChords = Convert.ToInt32(Console.ReadLine());
+                    if (numOfChords < 3)
+                    {
+                        Console.WriteLine("Please select a number greater then 3.");
+                        numOfChords = 0;
+                    }
                 }
-                else if (majorOrMinor.Equals("Minor") || majorOrMinor.Equals("minor"))
+                Chords[] chordProg = new Chords[numOfChords];
+                Boolean continueWhile = true;
+                while (continueWhile)
                 {
-                    isMajor = false;
-                    continueWhile = false;
+                    Console.WriteLine("Do you want Major triads, or Minor triads? Type 'Major' or 'Minor' depending on what you want");
+                    String majorOrMinor = Console.ReadLine();
+                    if (majorOrMinor.Equals("Major") || majorOrMinor.Equals("major"))
+                    {
+                        isMajor = true;
+                        continueWhile = false;
+                    }
+                    else if (majorOrMinor.Equals("Minor") || majorOrMinor.Equals("minor"))
+                    {
+                        isMajor = false;
+                        continueWhile = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid entry.");
+                    }
+                }
+                if (isMajor)
+                {
+                    Boolean selectedStarting = false;
+                    while (!selectedStarting)
+                    {
+                        Console.WriteLine("Select a starting chord from the following.");
+                        for (int i = 0; i < allMajorChordsStr.Length; i++)
+                        {
+                            Console.WriteLine(allMajorChordsStr[i]);
+                        }
+                        startingChord = Console.ReadLine();
+                        if (!allMajorChordsStr.Contains(startingChord))
+                        {
+                            Console.WriteLine("Invalid Chord.");
+
+                        }
+                        else
+                        {
+                            selectedStarting = true;
+                        }
+                    }
+                    Chords firstChord = chordMapMaj[startingChord];
+                    chordProg[0] = firstChord;
+                    Console.Write(chordProg[0].toString() + ", ");
+                    for (int i = 1; i < numOfChords - 1; i++)
+                    {
+                        chordProg[i] = chordProg[i - 1].nextChord();
+                        Console.Write(chordProg[i].toString() + ", ");
+                    }
+                    chordProg[numOfChords - 1] = chordProg[numOfChords - 2].nextChord();
+                    Console.WriteLine(chordProg[numOfChords - 1].toString());
+
                 }
                 else
                 {
-                    Console.WriteLine("Invalid entry.");
+                    Boolean selectedStarting = false;
+                    while (!selectedStarting)
+                    {
+                        Console.WriteLine("Select a starting chord from the following.");
+                        for (int i = 0; i < allMinorChordsStr.Length; i++)
+                        {
+                            Console.WriteLine(allMinorChordsStr[i]);
+                        }
+                        startingChord = Console.ReadLine();
+                        if (!allMinorChordsStr.Contains(startingChord))
+                        {
+                            Console.WriteLine("Invalid Chord.");
+
+                        }
+                        else
+                        {
+                            selectedStarting = true;
+                        }
+                    }
+                    Chords firstChord = chordMapMin[startingChord];
+                    chordProg[0] = firstChord;
+                    Console.Write(chordProg[0].toString() + ", ");
+                    for (int i = 1; i < numOfChords - 1; i++)
+                    {
+                        chordProg[i] = chordProg[i - 1].nextChord();
+                        Console.Write(chordProg[i].toString() + ", ");
+                    }
+                    chordProg[numOfChords - 1] = chordProg[numOfChords - 2].nextChord();
+                    Console.WriteLine(chordProg[numOfChords - 1].toString());
+                }
+                Console.WriteLine("Do you want to create another chord progression? Type 'Yes' for yes, or anything else for no.");
+                String ans = Console.ReadLine();
+                if (ans == "Yes" || ans == "yes" || ans == "Y" || ans == "y")
+                {
+                    continue;
+                }
+                else
+                {
+                    keepRunning = false;
+                    Console.Write("Thank you!");
                 }
             }
-            if (isMajor)
-            {
-                Boolean selectedStarting = false;
-                while (!selectedStarting)
-                {
-                    Console.WriteLine("Select a starting chord from the following.");
-                    for (int i = 0; i < allMajorChordsStr.Length; i++)
-                    {
-                        Console.WriteLine(allMajorChordsStr[i]);
-                    }
-                    startingChord = Console.ReadLine();
-                    if (!allMajorChordsStr.Contains(startingChord))
-                    {
-                        Console.WriteLine("Invalid Chord.");
-
-                    }
-                    else
-                    {
-                        selectedStarting = true;
-                    }
-                }
-                Chords firstChord = chordMapMaj[startingChord];
-                chordProg[0] = firstChord.toString();
-                Console.Write(chordProg[0] + ", ");
-                Console.Write(chordProg[0] + ", ");
-                for (int i = 1; i < numOfChords; i++)
-                {
-                    
-                    Console.Write(chordProg[i] + ", ");
-                }
-
-                
-            }
-            else
-            {
-                Boolean selectedStarting = false;
-                while (!selectedStarting)
-                {
-                    Console.WriteLine("Select a starting chord from the following.");
-                    for (int i = 0; i < allMinorChordsStr.Length; i++)
-                    {
-                        Console.WriteLine(allMinorChordsStr[i]);
-                    }
-                    startingChord = Console.ReadLine();
-                    if (!allMinorChordsStr.Contains(startingChord))
-                    {
-                        Console.WriteLine("Invalid Chord.");
-
-                    }
-                    else
-                    {
-                        selectedStarting = true;
-                    }
-                }
-            }
-
-            Console.WriteLine(isMajor);
-            Console.WriteLine(numOfChords);
-            Console.WriteLine(IMaj.nextChord());
-            Console.WriteLine(iMin.nextChord());
-
-
-
         }
     }
 }
